e4aadbb54bd6186cb479e3657fe47d0b04546f47
LYK45 <101735808+LYK45@users.noreply.github.com>
2023-06-27 21:25:48 -0300

    Add files via upload

---
+++ src/js/main.js
@@ -0,0 +1,119 @@
+// URL DA API DE DADOS
+URL = 'http://localhost:3000/produtos'
+//=================================================================================================
+// GET - Recupera todos os produtos e adiciona na tabela
+
+const produtoList = document.getElementById('produto-list');
+
+fetch(URL)
+    .then(res => res.json())
+    .then(produtos => {
+        let lista_produtos = '';
+        for (let i = 0; i < produtos.length; i++) {
+            vlt_total = produtos[i].qtd * produtos[i].vlr;
+            lista_produtos += `
+            <tr>
+                <th>${produtos[i].id}</th>
+                <td>${produtos[i].date}</td>
+                <td>${produtos[i].time}</td>
+                <td>${(parseFloat(produtos[i].vlr))}</td>
+                <td>
+                    <a onclick="getProduto(${produtos[i].id});" 
+                    class="btn btn-warning btn-sm" 
+                    data-toggle="modal" data-target="#produto-modal">
+                    <i class="fa fa-edit"></i>  Editar
+                    </a>
+
+                    <a onclick="$('#id-prod').text(${produtos[i].id});" class="btn btn-danger btn-sm" 
+                    data-toggle="modal" data-target="#modal-delete">
+                    <i class="fa fa-trash"></i> Remover
+                    </a>
+                </td>
+            </tr>
+            `;
+            produtoList.innerHTML = lista_produtos;
+        }
+    });
+//=================================================================================================
+
+// DELETE - PROCEDIMENTO PARA EXCLUIR UM PRODUTO
+const produtoDelete = document.getElementById('btn-delete');
+
+produtoDelete.addEventListener('click', (e) => {
+
+    let id = $('#id-prod').text();
+
+    fetch(`${URL}/${id}`, {
+        method: 'DELETE',
+    })
+    .then(res => res.json())
+    .then(() => location.reload());
+
+})
+//=================================================================================================
+
+// PROCEDIMENTO PARA RECUPERAR OS DADOS DE UM PRODUTO NA API
+function getProduto(id){
+
+    if(id == 0){
+        $('#edit-prod-id').text("");
+        $( "#produto-id" ).prop( "disabled", false );
+        $('#produto-id').val("");
+        $('#produto-date').val("");
+        $('#produto-time').val("");
+        $('#produto-vlr').val("");
+    }else{
+        $('#edit-prod-id').text(id);
+        fetch(`${URL}/${id}`).then(res => res.json())    
+        .then(data => {
+            $( "#produto-id" ).prop( "disabled", true );
+            $('#produto-id').val(data.id);
+            $('#produto-date').val(data.date);
+            $('#produto-time').val(data.time);
+            $('#produto-vlr').val(data.vlr);
+        });
+    }    
+}
+
+//=================================================================================================
+
+// CREATE or UPDATE - PROCEDIMENTO PARA CRIAR OU EDITAR UM PRODUTO
+
+const produtoForm = document.getElementById('produto-form');
+
+produtoForm.addEventListener('submit', (e) => {
+
+    // RECUPERA O ID DO PRODUTO
+    let id = parseInt($('#edit-prod-id').text());    
+
+    // RECUPERA OS DADOS DO PRODUTO
+    const produto = JSON.stringify({
+        id: document.getElementById('produto-id').value,
+        date: document.getElementById('produto-date').value,
+        time: document.getElementById('produto-time').value,
+        vlr: document.getElementById('produto-vlr').value
+    })
+
+    if (id >= 0) {
+        fetch(`${URL}/${id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: produto
+        })
+        .then(res => res.json())
+        .then(() => location.reload());  
+    }
+    else{ 
+        fetch(URL, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: produto
+        })
+        .then(res => res.json())
+        .then(() => location.reload());  
+    }      
+})
